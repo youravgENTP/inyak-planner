bd13c85af1acb429dd31ae4f4bf4ee7a3f7aaa79
you_nyung.gene <ssgeness12@gmail.com>
2026-08-16 01:12:13 +0900

    React router 사용환경 준비

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -20,12 +20,15 @@ AppShell과 TimetablePage 렌더링
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
 
